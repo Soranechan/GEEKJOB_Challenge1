de57b736a73d31eddb853af0b865ffd66fef4785
G21331085\Owner <miley.ayu-lovely@i.softbank.jp>
2017-06-14 17:02:42 +0900

    課題を提出します

---
+++ CampChallenge_java/007.標準クラス/クラス１.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+/**
+ *
+ * @author Owner
+ */
+
+
+ public class Challenge11{
+ public int i = 0;
+ public String s = "";
+ public void method(int x,String y){
+     this.i = x;
+     this.s =y;
+ }
+    
+ public void method2(){
+          
+    System.out.print(i+ "番目は"+s+"です。");
+ 
+}
+      
+}
+    
