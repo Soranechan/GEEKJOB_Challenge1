58c5462a94302a7c432820468baf225dfde50cff
G21331085\Owner <miley.ayu-lovely@i.softbank.jp>
2017-06-15 14:14:30 +0900

    課題提出

---
+++ CampChallenge_java/007.標準クラス/Blackjack.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BlackJack;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Owner
+ */
+public class NewClass1 {
+public abstract class Human{
+    abstract public int open(ArrayList<Integer>AL);
+    abstract public void setCard(ArrayList<Integer>AL);
+    abstract public boolean checkSum(ArrayList<Integer>AL);
+    ArrayList<Integer> mycards = new ArrayList<>();
+    
+}
+}
